eec256e45f92ce01220714646f4c351631fc3980
Richard Jones <richard@cottagelabs.com>
2025-01-27 14:35:39 +0000

    version bump for updated documentation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="coarnotify",
-    version="1.0.1.1",  # Version 1 of the library for the 1.0.1 spec
+    version="1.0.1.2",  # Version 2 of the library for the 1.0.1 spec
     packages=find_packages(),
     install_requires=[
         "requests"
